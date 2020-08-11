57bd5946b17c50980eea97595a1bf844de0c5e47
akhil0003 <akhil.singh0003@gmail.com>
2020-08-11 17:46:19 +1000

    Updates

---
+++ precache-manifest.15452573a433744b1a55d0c7022cbd59.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5b4d5393f77c2a132abfdc6a08814a7",
+    "url": "/indianlegalmatters/index.html"
+  },
+  {
+    "revision": "1a32f088feec90cc1c2c",
+    "url": "/indianlegalmatters/static/css/main.011a9c9e.chunk.css"
+  },
+  {
+    "revision": "88918e9e74f1d8bbcf65",
+    "url": "/indianlegalmatters/static/js/2.24e0c0dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/indianlegalmatters/static/js/2.24e0c0dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a32f088feec90cc1c2c",
+    "url": "/indianlegalmatters/static/js/main.a7454179.chunk.js"
+  },
+  {
+    "revision": "a2572173661ba03f005b",
+    "url": "/indianlegalmatters/static/js/runtime-main.248f2dd1.js"
+  },
+  {
+    "revision": "e5790b9d2d87e17bf055d8ab32d16654",
+    "url": "/indianlegalmatters/static/media/Raleway-Light.e5790b9d.woff"
+  },
+  {
+    "revision": "01e08cdcbed86623f1b831b15f8f5987",
+    "url": "/indianlegalmatters/static/media/mainpage.01e08cdc.jpg"
+  },
+  {
+    "revision": "b24b55f434e13381c4b800ac996c3972",
+    "url": "/indianlegalmatters/static/media/suneet.b24b55f4.jpg"
+  },
+  {
+    "revision": "75031ba2175b3d2cf4da1f7390dcd7b9",
+    "url": "/indianlegalmatters/static/media/whoweare.75031ba2.jpg"
+  }
+]);
